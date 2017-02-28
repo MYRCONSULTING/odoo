680ca3e35da572159998e48e8b61109092295e16
ricardo.livelli@gmail.com <ricardo.livelli@gmail.com>
2017-02-28 12:09:17 -0500

    Update Todo_Mega

--- custom-addons/todo_mega/todo_model.py
+++ custom-addons/todo_mega/todo_model.py
@@ -13,6 +13,7 @@ class ProjectProject(models.Model):
 class ProjectTask(models.Model):
     _inherit = ['project.task']
     x_survey_id = fields.Many2one('survey.survey',string='Encuesta')
+    x_survey_user_input_id = fields.Many2one('survey.user_input',string='Respuesta')
     
 class SurveyUserInput(models.Model):
     _inherit = ['survey.user_input']
