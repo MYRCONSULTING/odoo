3bd2cc4dc2d987eccd6b4d257f0b34d9c88b3213
ricardo <ricardo@ubuntu>
2017-04-22 15:49:23 -0500

    update

--- custom-addons/toodo/models/todo_model.py
+++ custom-addons/toodo/models/todo_model.py
@@ -13,14 +13,14 @@ class ProjectProject(models.Model):
 class ProjectTask(models.Model):
     _inherit = ['project.task']
     x_survey_id = fields.Many2one('survey.survey',string='Encuesta')
-    x_survey_user_input_id = fields.Many2many('survey.user_input',string='Respuesta')
     x_recursive = fields.Boolean('Recursive?')
     x_create_source = fields.Boolean('Create Source ?')
+    survey_user_input_ids = fields.Many2many('survey.user_input',string='Respuesta')
     
 class SurveyUserInput(models.Model):
     _inherit = ['survey.user_input']
     x_product_ids = fields.Many2one('product.product')
-    x_project_task_ids = fields.Many2one('project.task')
+    #project_task_ids = fields.Many2many('project.task')
     
 
 class ProjectTaskType(models.Model):
